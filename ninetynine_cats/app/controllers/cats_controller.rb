4c2faf7a28805ba29a912b10929b85d79b24ae01
alejandro <alejandroweil13@gmail.com>
2021-02-05 18:06:01 -0500

    Working on controller#new and create

--- ninetynine_cats/app/controllers/cats_controller.rb
+++ ninetynine_cats/app/controllers/cats_controller.rb
@@ -1,14 +1,47 @@
 class CatsController < ApplicationController 
     def index 
-        cats = Cat.all
-        render :cats 
+        @cats = Cat.all
+        render :index 
     end
 
     def show 
-        kitty = Cat.find(params[:id])
-            render :kitty 
+        @kitty = Cat.find(params[:id])
+        if @kitty
+            render :show
+        else
+            redirect_to cats_url
+        end
+        
     end
 
 
+    def new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            render :create
+        else
+            render json: @cat.errors.full_messages, status: :unprocessable_entity 
+        end
+    end
+
+
+
+    private
+    def cat_params
+        cat_params = params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
+    end
+
 end
 
+#  def method
+#     var = 3
+#     function
+#  end
+
+#  def function
+#     var + 1
+#  end
--- ninetynine_cats/db/seeds.rb
+++ ninetynine_cats/db/seeds.rb
@@ -9,15 +9,15 @@
 Cat.destroy_all
 ApplicationRecord.connection.reset_pk_sequence!('cats')
 
-Cat.create(birth_date: '2015/01/20', color: 'brown', name: 'whiskers', sex: 'M', description: 'im a fkn cat')
+Cat.create(birth_date: '2015/01/20', color: 'brown', name: 'Whiskers', sex: 'M', description: 'im a lovely cat')
 
-Cat.create(birth_date: '2005/01/20', color: 'pink', name: 'whiskers', sex: 'F', description: 'im a fkn cat')
-
-
-Cat.create(birth_date: '2000/01/20', color: 'grey', name: 'whiskers', sex: 'M', description: 'im a fkn cat')
+Cat.create(birth_date: '2005/01/20', color: 'pink', name: 'David', sex: 'F', description: 'im a adorable cat')
 
 
-Cat.create(birth_date: '2000/11/20', color: 'black', name: 'whiskers', sex: 'M', description: 'im a fkn cat')
+Cat.create(birth_date: '2000/01/20', color: 'grey', name: 'Rich', sex: 'M', description: 'im a fkn cat')
 
 
-Cat.create(birth_date: '2010/02/01', color: 'white', name: 'biscuit', sex: 'F', description: 'blabla')
+Cat.create(birth_date: '2000/11/20', color: 'black', name: 'Valerie', sex: 'M', description: 'im a cute cat')
+
+
+Cat.create(birth_date: '2010/02/01', color: 'white', name: 'Dane', sex: 'F', description: 'blabla')
